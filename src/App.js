5de0d2ae82d3d84c2a1506edfa238946da4dbe57
nathankelderman <nathanjkelderman@hotmail.com>
2018-12-03 13:04:48 -0500

    login screen

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
-import React, { Component } from 'react';
-import './App.css';
-import './components/login.css';
-import Login from './components/login.js';
+import React, { Component } from 'react'
+import './App.css'
+import './components/login.css'
+import { Button } from "react-bootstrap"
+import firebase from 'firebase'
 import database from './firebase.js'
 
 class App extends Component {
+
+  state = {
+    authenticated: false,
+    user: null
+  }
+
   render() {
     return (
       <div className="App">
-        <Login database = {database}/>
+        <div className="Login">
+          {this.state.authenticated ? <Button onClick={() => this.logout()}>Logout</Button> : <Button onClick={() => this.validateForm()}>Login</Button>}
+        </div>
+        <div className="Main">
+          {/* <MainScreen props/> */}
+        </div>
       </div>
     );
   }
-  
+
+  logout() {
+    database.auth().signOut()
+    .then(() => {
+      this.setState({
+        authenticated: false,
+        user: null
+      })
+    })
+  }
+
+  validateForm() {
+    var provider = new firebase.auth.GoogleAuthProvider()
+    database.auth().signInWithPopup(provider)
+    .then((result) => {
+        this.setState({
+            user: result.user,
+            authenticated: true
+        })
+    })
+  }
 }
 
 export default App;
